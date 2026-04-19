89bdac719bbf526dc5b9c9262ef8ec74c60e3d35
vvenyu <232058504+vvenyu@users.noreply.github.com>
2026-04-19 14:22:30 +0200

    Html layout and styled

---
+++ src/index.js
@@ -0,0 +1,21 @@
+import './styles.css';
+
+class Todo {
+  constructor(title, description, date, priority, checklist) {
+    this.title = title;
+    this.description = description;
+    this.date = date;
+    this.priority = priority;
+    this.checklist = checklist;
+  }
+
+
+}
+
+class Project {
+  constructor(name) {
+    this.name = name;
+  }
+
+}
+
